023649d100825811a42b5a4cbd08642a9581225d
Mihai <mihai@starcounter.com>
2017-10-19 14:18:37 +0200

    Initial commit

---
+++ RedirectExample/RedirectExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Starcounter;
+
+namespace RedirectExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            Application.Current.Use(new HtmlFromJsonProvider());
+            Application.Current.Use(new PartialToStandaloneHtmlProvider());
+
+            Handle.GET("/RedirectExample/test1", () =>
+            {
+                return new Test1Page();
+            });
+
+            Handle.GET("/RedirectExample/test2", () =>
+            {
+                var resp = new Response()
+                {
+                    StatusCode = 302
+                };
+                resp.Headers["Location"] = "/RedirectExample/test3";
+                return resp;
+            });
+
+            Handle.GET("/RedirectExample/test3", () =>
+            {
+                return new Test3Page();
+            });
+        }
+    }
+}
